bf546d990222799bf71d25557e8bd23bd7c3f7f3
Avdors <avdors@bk.ru>
2024-05-27 17:42:56 +0200

    add favorite top bar

--- app/src/main/java/com/example/jp_info_progect/ui_components/MainTopBar.kt
+++ app/src/main/java/com/example/jp_info_progect/ui_components/MainTopBar.kt
@@ -7,10 +7,15 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jp_info_progect.MainViewModel
 import kotlinx.coroutines.launch
 
 @Composable
-fun MainTopBar(title: String, scaffoldState: ScaffoldState) {
+fun MainTopBar(
+    mainViewModel: MainViewModel = hiltViewModel(), // для того чтобы обработать клик по избраному
+    title: String,
+    scaffoldState: ScaffoldState) {
 //меню мы можем открыть только черз корутину
     val corutine = rememberCoroutineScope()
     TopAppBar (
