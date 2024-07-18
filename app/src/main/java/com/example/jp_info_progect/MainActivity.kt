7110186a55b2c10c35675f05b5199491f842bdb5
Dmitry Volkov <99538385+Avdors@users.noreply.github.com>
2024-07-18 14:28:17 +0200

    add test 13

--- app/src/main/java/com/example/jp_info_progect/MainActivity.kt
+++ app/src/main/java/com/example/jp_info_progect/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             // task1
             val navController = rememberNavController()
+            val test = 13
             // заводим этот item чтобы в него складывать значение выбранного в Main Sreen itema
             var item: ListItem? = null
             JP_info_progectTheme {
